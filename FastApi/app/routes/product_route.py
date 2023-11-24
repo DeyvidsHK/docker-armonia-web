7ddf5e9357b944304616b8465db0f60bfd7ba414
deyvids.alvino <deyvids.alvino@digilab.pe>
2023-11-24 18:40:36 -0500

    Update product_route.py

--- FastApi/app/routes/product_route.py
+++ FastApi/app/routes/product_route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, BackgroundTasks
+from fastapi import APIRouter
 from app.schemas.product_schemas import get_product_db, GetProduct, CreateProduct, create_product_db, ResponseCreateProduct
 
 product_route = APIRouter()
@@ -8,9 +8,5 @@ async def get_product():
     return get_product_db()
 
 @product_route.post('/CreateProduct', response_model=ResponseCreateProduct, summary="Crear productos")
-async def create_product(productData: CreateProduct, background_tasks: BackgroundTasks):
-    # No es necesario leer el contenido de la imagen, ya que ya está en bytes
-    image_content = productData.imagen
-    
-    # Pasa image_content a la función create_product_db
-    return create_product_db(productData, image_content)
+async def create_product(productData: CreateProduct):
+    return create_product_db(productData)
